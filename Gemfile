5d092fad6b031e34832cad1ef2e8cd676a95a801
Chris Boyle <chris@bbck.net>
2013-03-04 23:34:43 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+ruby   '1.9.3'
+source 'https://rubygems.org'
+
+gem 'rails',          '3.2.12'
+gem 'pg',             '~> 0.14.1'
+gem 'jquery-rails',   '~> 2.2.1'
+
+group :assets do
+  gem 'sass-rails',     '~> 3.2.3'
+  gem 'bootstrap-sass', '~> 2.3.0.1'
+  gem 'coffee-rails',   '~> 3.2.1'
+  gem 'uglifier',       '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'heroku'
+  gem 'pry'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DragonTracker::Application.config.secret_token = 'd9acdb16f586f11c4d44c87490d5f02aeec3a8d1e971f80ae885848106c8c2bbf5b16bfbe8a7e25792908a09713bf517c746d211b84c143697acee68c26ab857'
