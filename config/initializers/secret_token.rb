f6511632e5bdf1f1391726125fc91aedb37f2559
Chris Boyle <chris@bbck.net>
2013-03-05 21:31:46 -0500

    New secret token that's really really secret.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DragonTracker::Application.config.secret_token = 'd9acdb16f586f11c4d44c87490d5f02aeec3a8d1e971f80ae885848106c8c2bbf5b16bfbe8a7e25792908a09713bf517c746d211b84c143697acee68c26ab857'
+DragonTracker::Application.config.secret_token = ENV["SECRET_TOKEN"]
